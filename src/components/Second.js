9b483bd98dbeb9d56d99a2a5569dc509ed2bd631
Bina87 <86130299+Bina87@users.noreply.github.com>
2021-07-08 13:42:04 +0200

    first commit

---
+++ src/components/Second.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Second = () => {
+    return (
+        <div style={{backgroundColor:"green"}}>
+            <p>this is my first app react </p>
+        </div>
+    )
+}
+
+export default Second
